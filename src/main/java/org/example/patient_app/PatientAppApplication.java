0b7415d8add25b09082067a0f2a9d47a3e52c83d
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-03-15 17:05:28 +0000

    create de repository & entity.

--- src/main/java/org/example/patient_app/PatientAppApplication.java
+++ src/main/java/org/example/patient_app/PatientAppApplication.java
@@ -1,13 +1,22 @@
 package org.example.patient_app;
 
+import org.example.patient_app.entities.Patient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PatientAppApplication {
-
+public class PatientAppApplication implements CommandLineRunner {
+    public Patient patient;
     public static void main(String[] args) {
         SpringApplication.run(PatientAppApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+     patient.save();
+
+    }
 }
---
+++ src/main/java/org/example/patient_app/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package org.example.patient_app.repository;
+
+import org.example.patient_app.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
+
+
+public interface PatientRepository extends JpaRepository {
+
+}
